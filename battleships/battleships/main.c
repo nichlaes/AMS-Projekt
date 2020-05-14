cff5ec8eee52f0f7b32caadadec1ce1fc3e27e4b
Nichlaes <nichlaes.soerensen@gmail.com>
2020-05-14 17:47:46 +0200

    updated main, moved switch-case inside interrupt

--- battleships/battleships/main.c
+++ battleships/battleships/main.c
@@ -19,45 +19,34 @@ Player p1;
 Player p2;
 Point Shot;
 
-int isStateHandled = 1;
-
 int main(void)
 {	
 
 	p1.name[0] = 'P';
 	p1.name[1] = '1';
 	p1.name[2] = '\0';
-	p1.smallShip[0].x = 0;
+	p1.smallShip[0].x = 0; //Remember to set the hole ship!!!!!
 	p1.smallShip[0].y = 0;
 	p1.mediumShip[0].x = 0;
 	p1.mediumShip[0].y = 1;
 	p1.BigShip[0].x = 0;
 	p1.BigShip[0].y = 2;
-	Point shot1 = {1,1,1};
-	p1.Shots[0] = shot1;
-	Point shot2 = {3,1,0};
-	p1.Shots[1] = shot2;
-	p1.shotsFired = 2;
+	p1.shipsFieldsLeft = 1;
 	
 	p2.name[0] = 'P';
 	p2.name[1] = '2';
-	p2.smallShip[0].x = 2;
-	p2.smallShip[0].y = 1;
-	p2.mediumShip[1].x = 3;
-	p2.mediumShip[1].y = 3;
-	
+	p2.smallShip[0].x = 0;
+	p2.smallShip[0].y = 0;
+	p2.mediumShip[0].x = 0;
+	p2.mediumShip[0].y = 1;
+	p2.BigShip[0].x = 0;
+	p2.BigShip[0].y = 2;
+	p2.shipsFieldsLeft = 1;
 	
 	DisplayInit();
-	//initIRQInterrupt();
-	sei(); // Global interrupt enable
 	TouchDriverInit();
-	//handleGameOverState();
-	//Shot = {3,3,0};
-	DisplayInit();
-	//initIRQInterrupt();
+	initIRQInterrupt();
 	sei(); // Global interrupt enable
-
-	TouchDriverInit();
 	//DrawBackground();
 	//int c=1;
 	//int length = snprintf( NULL, 0, "%d", c );
@@ -67,30 +56,8 @@ int main(void)
 	//DrawText(str, 10,10,1);
 	//DrawShip(4, 5,5);
 	DisplayOn();
-	handleAttackState();
     while (1) 
     {
-		if(isStateHandled == 0)
-		{
-			switch(GetGameState()){
-				case IdleState:
-				handleIdleState();
-				break;
-				case AttackState:
-				handleAttackState();
-				break;
-				case EndState:
-				handleEndState();
-				break;
-				case GameOverState:
-				//handleGameOverState();
-				break;
-				default:
-				break;
-			}
-			isStateHandled = 1;
-			EIMSK |= 0b00010000;	
-		}
     }
 }
 
@@ -99,36 +66,31 @@ int main(void)
 ISR (INT4_vect)
 {
 	EIMSK &= ~(0b00010000);
-	//DrawText("t",(i++)*16,(i++)*16,1);
-	//unsigned int dataX = readTouchXInput();
-	//unsigned int dataY = readTouchYInput();
-	//dataX = readTouchXInput();
-	//dataY = readTouchYInput();
-	//DisplayOff();
-	_delay_ms(10);
 	unsigned int y = GetYPosition();
 	unsigned int x = GetXPosition();
 	y= GetYPosition();
 	x = GetXPosition();
-	int xkoord = GetMapXKoord(x);
-	int ykoord  = GetMapYKoord(y);
-	if((xkoord == 0 || xkoord == 1 || xkoord == 2 || xkoord == 3)&&(ykoord == 0 || ykoord == 1 || ykoord == 2 || ykoord == 3 || ykoord == 4)){
-		DrawShip(2,ykoord, xkoord);
-		//int length = snprintf( NULL, 0, "%d", dataX );
-		//char* str = malloc( length + 1 );
-		//snprintf( str, length + 1, "%d", dataX );
-		//DrawText(str,5,5,1);
-		//free(str);
-//
-		//int lengthy = snprintf( NULL, 0, "%d", dataY );
-		//char* stry = malloc( lengthy + 1 );
-		//snprintf( stry, lengthy + 1, "%d", dataY );
-		//DrawText(stry,30,30,1);
-		//free(stry);
+	Shot.y = GetMapXKoord(x); // opposite
+	Shot.x = GetMapYKoord(y); // opposite
+	
+	switch(GetGameState()){
+		case IdleState:
+		handleIdleState();
+		break;
+		case AttackState:
+		handleAttackState();
+		break;
+		case EndState:
+		handleEndState();
+		break;
+		case GameOverState:
+		handleGameOverState();
+		break;
+		default:
+		break;
 	}
-	isStateHandled = 0;
-	//EIMSK |= 0b00010000;
-
+	_delay_ms(500);
+	EIMSK |= 0b00010000;
 }
 
 
@@ -155,8 +117,9 @@ void handleEndState(){
 		shotHit = TakeShot(&p1, Shot, &p2);
 	} else shotHit = TakeShot(&p2, Shot, &p1);
 	
-	//If shot is 
+	//If shot is valid
 	if (shotHit != -1){
+		DrawShot(Shot);
 		if(p1.shipsFieldsLeft == 0 || p1.shipsFieldsLeft == 0){
 			EndGame();
 		} else NextState();	
@@ -179,6 +142,8 @@ void handleIdleState(){
 	//strcpy(name, "s turn");
 	DrawText(name, 50,50, 2);
 	DrawText("tap to continue", 50, 150, 1);
+	NextState();
+}
 	
 void handleGameOverState(){
 	Player *player;
@@ -207,5 +172,5 @@ void handleGameOverState(){
 	free(str);
 	free(str1);
 	
-	}
 }
+
